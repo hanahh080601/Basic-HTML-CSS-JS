3a6f24be858e8da33054cbc6fb05a72d1f371514
hanahh080601 <hanlehngoc080601@gmail.com>
2021-03-29 21:39:29 +0700

    js

---
+++ javascript/javascipt.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("add")
+// let nameCheck = document.getElementById("name").oninput.length
+// let data = new Array();
+// for(i = 0; i < nameCheck; i++)
+// {
+
+// }
+console.log(button)
+button.onclick = () => {
+  
+  
+    alert("Input");
+  
+}
